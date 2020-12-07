98fc4180c86d475dec700fadc24cbcfc5dd625e1
A.OULD <72967617+Adom7@users.noreply.github.com>
2020-12-07 15:49:29 +0100

    recup données Json fait

--- src/seed.js
+++ src/seed.js
@@ -1,17 +1,34 @@
 const knex = require('knex')
 const dotenv = require ('dotenv')
+const https = require ('https');
+const username = null || 'Amadou00'
 
+function RequestUsername(username){
+    const options = {
+        host: 'api.github.com',
+        path: '/users/' + username,
+        method: 'GET',
+        headers: {'user-agent': 'node.js'}
+        };
+        
+        let request = https.request(options, function(response){
+        let body = '';
+        response.on("data", function(chunk){
+            body += chunk.toString('utf8');
+        });
+        
+        response.on("end", function(){
+            console.log("Body: ", body);
+            });
+        });
+        
+        request.end();
+}
 
-
-// fetch('https://api.github.com/users/Adom7')
-// .then(res => res.json())
-// .then(data => {
-//     console.log(data);
-// })
 
 
 dotenv.config()
-module.exports = function(){
+function ServerKnex(){
     const {DATABASE_HOST : host , DATABASE_NAME : name, DATABASE_USER : user , DATABASE_PORT : port, DATABASE_PASS : pass} = process.env
     return pg = knex({
         client: 'pg',
@@ -25,3 +42,5 @@ module.exports = function(){
         searchPath: ['knex', 'public'],
       });
 }
+
+module.exports = {Request,ServerKnex}
--- src/serverExpress.js
+++ src/serverExpress.js
@@ -1,8 +1,10 @@
 const express= require ('express')
 const dotenv = require('dotenv')
 const database = require('./seed')
+// import RequestUsername from './seed'
 
-database()
+
+// database()
 dotenv.config()
 
 const ServerPort = process.env.SERVER_PORT ||   4242
@@ -12,6 +14,10 @@ app.get('/', (req,res) =>{
     res.json({
         hello:'From Pokedex API'
     })
+})
+
+app.get('user/:username' , (req,res) => {
+  res.send(req.params.username)
 })
 
 app.listen(ServerPort, ()=>{
